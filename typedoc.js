d93dd66a32a67c2f2df086b8befe7bd72e4e8e48
Chris Milson <chris.w.milson@gmail.com>
2021-02-25 16:16:03 +0900

    Use typedoc to generate docs

---
+++ typedoc.js
@@ -0,0 +1,16 @@
+const { getPackagesSync } = require('@lerna/project')
+const path = require('path')
+
+module.exports = {
+  name: 'Structured',
+  out: 'docs',
+  theme: 'default',
+  exclude: '*.spec.ts',
+  'external-modulemap': '.*packages/([^/]+)/.*',
+  entryPoints: getPackagesSync(path.join(__dirname, 'packages')).map(
+    pkg => path.join(
+      path.relative(__dirname, pkg.location), 'src/index.ts'
+    )
+  ),
+  excludeExternals: false
+}

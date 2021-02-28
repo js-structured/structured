3c62d2a287a7c84d6884b7d629bfbf351c28cd33
Chris Milson <chris.w.milson@gmail.com>
2021-02-28 14:51:25 +0900

    Enforce type on empty array

--- packages/iterable/src/__tests__/merge.test.ts
+++ packages/iterable/src/__tests__/merge.test.ts
@@ -3,7 +3,7 @@ import { merge } from '..'
 describe('merge', () => {
   const a = [1, 3, 5, 7, 9]
   const b = [2, 4, 4.5, 6, 8]
-  const c = []
+  const c: number[] = []
 
   const concatenated = a.concat(b).concat(c)
   const merged = [...merge<number>((a, b) => a - b, a, b, c)]
